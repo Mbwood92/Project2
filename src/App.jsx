a2fc0c50fcb61aa455328ccda382e5ff6a52d173
mbwood92 <malikabythewood4@gmail.com>
2023-11-27 13:37:03 -0500

    pages changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,18 @@ import Nav from './components/Nav';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+ 
+// you need to have <Routes></Routes> (this is plural because it wraps all of the routes)
+  // (this is singular because it's describing one) around each <Route />
+  // <Route /> has to have the path - which tells us what it will look like in the address bar
+  //    and element - which tells us which page or component will load
+return (
     <div className='App'>
       <Nav />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/episodes' element={<Episodes/>} />
-        <Route path='/characters/:symbol/' element={<Characters/>} />
+        <Route path='/characters' element={<Characters/>} />
 
       </Routes>
       </div>
--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,10 +1,64 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-
 function Characters() {
+    // make a variable to store our api key
+    //const apiKey = "8B45521A-E096-4E27-B0C8-17A173D1060F";
+    // get the currency symbol from the url
+    // these two lines get the params object from the url (address bar), 
+    // and then we save into a variable called symbol for us to use later
+    const params = useParams();
+    // console.log(params);
+    const symbol = params.symbol;
+    // use the apiKey and symbol variables to make our url
+    const baseUrl = `https://rickandmortyapi.com/api/`;
+
+    // state to hold the character data
+    const [cartoon, setCartoon] = useState("null");
+
+    // function to fetch character data
+    const getCartoon = async () => {
+        try {
+            console.log(url)
+            const response = await fetch(`${baseUrl}/characters/`);
+            const data = await response.json();
+            setCartoon(data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    // useEffect to run getCartoon when component mounts
+    useEffect(() => {
+        getCartoon();
+    }, []);
+
+    // loaded function shows up when the data has been fetched
+    const loaded = () => {
+        return (
+            <div>
+                <h1>{cartoon.Characters}</h1>
+                <h2>{cartoon.Info}</h2>
+                <img src={cartoon.Poster} alt={cartoon.Title} />
+                <h2>{cartoon.Location}</h2>
+                <h3>Ratings</h3>
+                {cartoon.data.map (data => {
+                    return (<><h4>{data.Source}</h4>
+                    <h4>{data.Value}</h4></>)
+                })  }
+            </div>
+        )
+    }
+
+    // function for when data doesn't exist yet
+    const loading = () => {
+        return <h1>Loading...</h1>
+    }
+
+    // if cartoon has data - so if cartoon is not null
+
   return (
-    <div>Characters</div>
+    cartoon && cartoon.data ? loaded() : loading()
   )
 }
 
